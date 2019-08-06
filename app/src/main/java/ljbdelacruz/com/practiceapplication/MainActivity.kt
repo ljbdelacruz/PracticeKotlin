0e899f176c9c4c442213e3e61182f2c2f7195b10
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-08-07 06:57:24 +0800

    intent show for forgot password

--- app/src/main/java/ljbdelacruz/com/practiceapplication/MainActivity.kt
+++ app/src/main/java/ljbdelacruz/com/practiceapplication/MainActivity.kt
@@ -16,14 +16,19 @@ class MainActivity : AppCompatActivity() {
         val usernameTF=findViewById<EditText>(R.id.login_usernameTF);
         val passwordTF=findViewById<EditText>(R.id.login_passwordTF);
         val loginButton=findViewById<Button>(R.id.login_loginBtn);
+        val forgotPasswordButton=findViewById<Button>(R.id.login_forgotPasswordBtn)
         window.decorView.apply {
             systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         }
-
         loginButton.setOnClickListener{
             this.processLogin(username = usernameTF.text.toString(), password = passwordTF.text.toString())
         }
+        forgotPasswordButton.setOnClickListener{
+            this.forgotPassword();
+        }
     }
+
+
     fun processLogin(username:String, password:String){
         //do oauth here
         val intent = Intent(this, Dashboard::class.java)
