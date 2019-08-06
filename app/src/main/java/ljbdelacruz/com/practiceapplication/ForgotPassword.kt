a0e5c5ccb6be85167ac2d737aeba7fc852a8fef5
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-08-07 06:54:17 +0800

    enhancements

--- app/src/main/java/ljbdelacruz/com/practiceapplication/ForgotPassword.kt
+++ app/src/main/java/ljbdelacruz/com/practiceapplication/ForgotPassword.kt
@@ -2,11 +2,22 @@ package ljbdelacruz.com.practiceapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class ForgotPassword : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+        val emailTF=findViewById<EditText>(R.id.fp_emailTF);
+        val rButton=findViewById<Button>(R.id.fp_resetPasswordBtn);
+        rButton.setOnClickListener{
+            this.resetPassword();
+        }
+    }
+
+    fun resetPassword(){
+
     }
 }
