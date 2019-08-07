d4dcf3e1fc0239087016a5bbeefc4fcc7703d508
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-08-07 12:26:26 +0800

    setup th setting for retrofit networking

--- app/src/main/java/ljbdelacruz/com/practiceapplication/Services/Rest/IRentMeWebService.kt
+++ app/src/main/java/ljbdelacruz/com/practiceapplication/Services/Rest/IRentMeWebService.kt
@@ -1,14 +1,24 @@
 package ljbdelacruz.com.practiceapplication.Services.Rest
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.experimental.CoroutineCallAdapterFactory
+import ljbdelacruz.com.practiceapplication.Services.Models.RMCategory
+import kotlinx.coroutines.Deferred
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
 
+
+const val baseURL = "http://localhost:8080/"
+
 interface IRentMeWebService {
     //#region Get Category
     @GET("/category/list/all")
-    fun getCategoryByParentID(@Query("id") id : Int)
+    fun getCategoryByParentID(@Query("id") id : Int):Deferred<RMCategory>
 
     //#endregion
     //#region Post Category
@@ -16,4 +26,23 @@ interface IRentMeWebService {
 //    fun newCategory(@Body("data") data: )
 
     //#endregion
+
+    companion object {
+        operator fun invoke():IRentMeWebService{
+            val interceptor=Interceptor{chain ->
+                //put additional parameters here
+//                val url = chain.request().url().newBuilder().build()
+                val request=chain.request().newBuilder().build();
+                return@Interceptor chain.proceed(request);
+            }
+            val okHttpClient=OkHttpClient.Builder().addInterceptor(interceptor).build()
+            return Retrofit.Builder().client(okHttpClient).baseUrl(baseURL).addCallAdapterFactory(
+                CoroutineCallAdapterFactory()).addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(IRentMeWebService::class.java);
+        }
+    }
+
+
+
 }
